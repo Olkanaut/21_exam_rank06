d8d4999cd7822df6291e2e247bdff752a5444f4c
olkanaut <okhuraskina@gmail.com>
2021-07-12 17:49:43 +0300

    opt

--- mini_serv.c
+++ mini_serv.c
@@ -4,11 +4,13 @@
 #include <string.h>
 #include <netinet/ip.h>
 
-int id_by_sock[65536];
+int ids[65536];
 int max_sock = 0;
-int next_id = 0;
-fd_set active_socks, rfr, rfw;
-char buf_read[42*4096], buf_write[42*4096 + 11];
+int serverFD;
+int idCount = 0;
+fd_set	fds_active, fds_read, fds_write;
+char	buf_read[42*4096], buf_write[42*4096 + 11];
+struct sockaddr_in addr;
 
 void	fatal_exit()
 {
@@ -20,14 +22,29 @@ void	send_msg(int author)
 {
 	for (int sock = 0; sock <= max_sock; sock++)
 	{
-		if (FD_ISSET(sock, &rfw) && sock != author)
+		if (FD_ISSET(sock, &fds_write) && sock != author)
 			send(sock, buf_write, strlen(buf_write), 0);
 	}
 }
 
+int accept_client()
+{
+	socklen_t addr_len = sizeof(addr);
+	int clientFd = accept(serverFD, (struct sockaddr*)&addr, &addr_len);
+	if (clientFd < 0)
+		return 1;
+
+	max_sock = (clientFd > max_sock) ? clientFd : max_sock;
+	ids[clientFd] = idCount++;
+	FD_SET(clientFd, &fds_active);
+
+	sprintf(buf_write, "server: client %d just arrived\n", ids[clientFd]);
+	send_msg(clientFd);
+	return 0;
+}
+
 int main(int ac, char **av)
 {
-	// fd_set active_socks, rfr;
 	char  buf_str[42*4096];
 
 	if (ac != 2)
@@ -37,71 +54,63 @@ int main(int ac, char **av)
 	}
 	int port = atoi(av[1]); //(void)port
 
-	bzero(&id_by_sock, sizeof(id_by_sock));
-	FD_ZERO(&active_socks);
+	bzero(&ids, sizeof(ids));
+	FD_ZERO(&fds_active);
 
-	int server_sock = socket(AF_INET, SOCK_STREAM, 0);
-	if (server_sock < 0)
+	serverFD = socket(AF_INET, SOCK_STREAM, 0);//was local
+	if (serverFD < 0)
 		fatal_exit();
 
-	max_sock = server_sock;
-	FD_SET(server_sock, &active_socks);
+	max_sock = serverFD;
+	FD_SET(serverFD, &fds_active);
 
-	struct sockaddr_in addr;
+	// struct sockaddr_in addr;//memset(&_sockAddr, 0, sizeof(_sockAddr));
 	addr.sin_family = AF_INET;
 	addr.sin_addr.s_addr = (1 << 24) | 127;//htonl(2130706433);
-	addr.sin_port = (port >> 8) | (port << 8);//htons(port);
+	addr.sin_port = htons(port);
 	socklen_t addr_len = sizeof(addr);
 
-	if ((bind(server_sock, (const struct sockaddr*)&addr, sizeof(addr))) < 0)
+	if (bind(serverFD, (const struct sockaddr*)&addr, addr_len))
 		fatal_exit();
-	if (listen(server_sock, SOMAXCONN) < 0)//if (listen(sock_fd, 0) < 0)
+	if (listen(serverFD, SOMAXCONN))//SOMAXCONN or 0//150
 		fatal_exit();
 
 	while (1)
 	{
-		rfr = rfw = active_socks;
-		if (select(max_sock + 1, &rfr, &rfw, NULL, NULL) < 0)
+		fds_read = fds_write = fds_active;
+		if (select(max_sock + 1, &fds_read, &fds_write, NULL, NULL) < 0)
 			continue ;
 
 		for (int sock = 0; sock <= max_sock; sock++)
 		{
-			if (FD_ISSET(sock, &rfr) && sock == server_sock)
+			if (FD_ISSET(sock, &fds_read) == 0)
+				continue ;
+
+			if (sock == serverFD)
 			{
-				int client_sock = accept(server_sock, (struct sockaddr*)&addr, &addr_len);
-				if (client_sock < 0)
+				if (accept_client())
 					continue ;
-
-				max_sock = (client_sock > max_sock) ? client_sock : max_sock;
-				id_by_sock[client_sock] = next_id++;//////
-				FD_SET(client_sock, &active_socks);
-
-				sprintf(buf_write, "server: client %d just arrived\n", id_by_sock[client_sock]);
-				send_msg(client_sock);
 				break ;
 			}
 
-			if (FD_ISSET(sock, &rfr) && sock != server_sock)
+			int read_bytes = recv(sock, buf_read, 42*4096, 0);
+			if (read_bytes <= 0)
 			{
-				int read_bytes = recv(sock, buf_read, 42*4096, 0);
-				if (read_bytes <= 0)
+				sprintf(buf_write, "server: client %d just left\n", ids[sock]);
+				send_msg(sock);
+				FD_CLR(sock, &fds_active);
+				close(sock);//
+				break ;
+			}
+			for (int i = 0, j = 0; i < read_bytes; i++, j++)
+			{
+				buf_str[j] = buf_read[i];
+				if (buf_str[j] == '\n')
 				{
-					sprintf(buf_write, "server: client %d just left\n", id_by_sock[sock]);
+					buf_str[j] = '\0';
+					sprintf(buf_write, "client %d: %s\n", ids[sock], buf_str);
 					send_msg(sock);
-					FD_CLR(sock, &active_socks);
-					close(sock);//
-					break ;
-				}//else
-				for (int i = 0, j = 0; i < read_bytes; i++, j++)
-				{
-					buf_str[j] = buf_read[i];
-					if (buf_str[j] == '\n')
-					{
-						buf_str[j] = '\0';
-						sprintf(buf_write, "client %d: %s\n", id_by_sock[sock], buf_str);
-						send_msg(sock);
-						j = -1;////
-					}
+					j = -1;////
 				}
 			}
 		}
